c04bc4ba1b8c54ffd4180ecb95c9d3bba814a36c
Roshan Jhendi <rosn030214@gmail.com>
2026-04-15 21:55:09 +0545

    video testimonial admin edit page fix

--- resources/views/admin/video-testimonial/edit.blade.php
+++ resources/views/admin/video-testimonial/edit.blade.php
@@ -1,42 +1,45 @@
 @extends('layouts.admin')
 @section('content')
 
-	 <div class="content">
+    <div class="content">
 
 
-<div class="page-header">
-    <div class="breadcrumb-line">
-        <ul class="breadcrumb">
-            <li>Edit  </li>
-        </ul>
-        <ul class="breadcrumb-elements">
+        <div class="page-header">
+            <div class="breadcrumb-line">
+                <ul class="breadcrumb">
+                    <li>Edit </li>
+                </ul>
+                <ul class="breadcrumb-elements">
 
-        </ul>
-        <a class="breadcrumb-elements-toggle"><i class="icon-menu-open"></i></a><a class="breadcrumb-elements-toggle"><i class="icon-menu-open"></i></a></div>
-</div>
-<div class="content">
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <div class="panel-title">
-
+                </ul>
+                <a class="breadcrumb-elements-toggle"><i class="icon-menu-open"></i></a><a
+                    class="breadcrumb-elements-toggle"><i class="icon-menu-open"></i></a>
             </div>
-            {{ Form::model($information, ['method' => 'patch', 'action' => ['Admin\VideoTestimonialController@update', $information->id],'files' => true]) }}
-                        @include('admin.video-testimonial.form')
-
-                        {{ Form::close() }}
-                </div>
-
-
-
-</div>
         </div>
-    </div>
-    </div>
+        <div class="content">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <div class="panel-title">
+
+                            </div>
+                            {{ Form::model($information, ['method' => 'patch', 'route' =>
+                            ['video-testimonial.update', $information->id],'files' => true]) }}
+                            @include('admin.video-testimonial.form')
+
+                            {{ Form::close() }}
+                        </div>
+
 
 
                     </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 
 
 
